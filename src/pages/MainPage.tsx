8a383eae53105f3b1f074ad3bf477a9bf74f76bc
mynameSumin <msm4167@naver.com>
2025-04-05 22:55:04 +0900

    feat: 이미지 클릭 시 반응형 애니메이션 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,28 +1,53 @@
 import "../styles/Main.css";
-import { useMemo, useEffect } from "react";
+import { useMemo, useEffect, useRef, useState } from "react";
 import { generateStars, animateStars } from "../utils/starAnimation";
-import { animateName, animateImage } from "../utils/animeAnimation";
+import {
+  animateName,
+  animateImage,
+  clickCharacter,
+  resizeLetterSpace
+} from "../utils/animeAnimation";
 import myCharacter from "../assets/myCharacter.png";
 
 function HomePage() {
   const stars = useMemo(() => generateStars(80), []);
-  
+  const isInitialRender = useRef(true);
+  const [isClicked, setIsClicked] = useState(false);
+  const handleResize = () : any => {
+    const width = window.innerWidth;
+    if (isInitialRender.current) {
+        isInitialRender.current = false;
+        return;
+      }
+
+    if(isClicked) {
+        console.log("resize!");
+        resizeLetterSpace(width);
+    }
+  };
+
   useEffect(() => {
     animateStars(stars);
-    
     const height = window.innerHeight;
     animateName(height);
     animateImage();
-   
   }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isClicked]);
 
   return (
     <div className="bg-sky-950 w-screen h-screen overflow-hidden relative">
-        <header className="columns-3 mt-6 font-medium flex justify-center gap-[15%]">
-    <div className="text-3xl opacity-0">ABOUT ME</div>
-    <div className="text-3xl opacity-0">PROJECTS</div>
-    <div className="text-3xl opacity-0">CONTACT</div>
-        </header>
+      <header className="columns-3 mt-6 font-medium flex justify-center gap-[16.5%]">
+        <div className="text-3xl">ABOUT ME</div>
+        <div className="text-3xl">PROJECTS</div>
+        <div className="text-3xl">CONTACT</div>
+      </header>
       {stars.map((star, idx) => (
         <div
           key={idx}
@@ -31,19 +56,24 @@ function HomePage() {
             {
               "--x": star.x,
               "--y": star.y,
-              opacity: 0
+              opacity: 0,
             } as React.CSSProperties
           }
         />
       ))}
       <div>
-        <div className="name fixed bottom-1/2 left-1/2 translate-x-[-50%] max-w-1/2 font-extrabold opacity-0">
-          <span className="text-cyan-700">MIN</span>
-          <span className="text-blue-200">SU</span>
-          <span className="text-cyan-700">MIN</span>
+        <div className="name absolute inset-0 flex justify-center items-center font-extrabold opacity-0 sm:justify-center">
+          <div className="letter text-cyan-700 inline-block">MIN</div>
+          <div className="letter text-blue-200 inline-block">SU</div>
+          <div className="letter text-cyan-700 inline-block">MIN</div>
         </div>
+
         <img
-          className="image fixed bottom-1/2 left-1/2 translate-x-1/2 translate-y-1/2  opacity-0 w-1/50  md:w-1/60 lg:w-1/100 hover:scale-102"
+          className="image pointer-events-auto fixed bottom-1/2 left-1/2 translate-x-1/2 translate-y-1/2  opacity-0 w-1/50  md:w-1/70 lg:w-1/100 hover:scale-102"
+          onClick={() => {
+            clickCharacter(window.innerWidth);
+            setIsClicked(true);
+        }}
           src={myCharacter}
           alt="Character"
         />
--- src/utils/animeAnimation.ts
+++ src/utils/animeAnimation.ts
@@ -6,8 +6,8 @@ export const animateName = (height: number) => {
       targets: ".name",
       scale: 5,
       translateY: -height * 0.06,
-      delay: 700,
-      duration: 5000,
+      delay: 1000,
+      duration: 3000,
       opacity: 1,
       easing: "easeOutElastic",
     });
@@ -19,10 +19,45 @@ export const animateName = (height: number) => {
     anime({
       targets: ".image",
       scale: 20,
-      delay: 700,
-      duration: 5000,
+      delay: 1000,
+      duration: 3000,
       opacity: 1,
       easing: "easeOutElastic",
     });
   };
+
+  export const clickCharacter = (width: number) : any => {
+    anime({
+        targets: ".name",
+        letterSpacing: ["0px", `${width * 0.01}px`],
+        translateX: 6,
+        duration: 1500,
+        easing: "easeInOutExpo"
+    });
+    anime({
+        targets: ".image",
+        translateX: width * - 0.011,
+        duration: 1500,
+        easing: "easeInOutExpo"
+    })
+  }
+
+  export const resizeLetterSpace = (width: number) : any => {
+    const currentLetterSpacing = parseFloat(getComputedStyle(document.querySelector(".name")!).letterSpacing) || 0;
+    const newLetterSpacing = width * 0.008; // 기존 값에서 조금 줄어듦
+
+    anime({
+        targets: ".name",
+        letterSpacing: [`${currentLetterSpacing}px`, `${newLetterSpacing}px`],
+        translateX: 6,
+        duration: 800,
+        easing: "easeOutQuad"
+    });
+
+    anime({
+        targets: ".image",
+        translateX: width * - 0.011,
+        easing: "easeOutQuad"
+    })
+  }
   
