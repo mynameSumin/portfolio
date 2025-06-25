b33e77c0f49b8e694d7077aee8106007ee06f3b0
mynameSumin <msm4167@naver.com>
2025-06-25 13:56:19 +0900

    feat: contact 화면 애니메이션 구현

---
+++ src/utils/contactAnimation.ts
@@ -0,0 +1,14 @@
+import anime from "animejs";
+
+//click to start button
+export const startAnimation = () => {
+    anime({
+        targets: ".start-content span",
+        opacity: [0, 1],
+        translateY: [-3, 0],
+        loop: true,
+        delay: function(el, i) {
+            return i * 200 + 400;
+        }
+    })
+}
